2ce875f1d66881ec9f36c35a83e53f1bf7718fdc
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2026-03-11 17:18:17 +0530

    Upload the files
    Uplaod the files

---
+++ uploads/files.js
@@ -0,0 +1,14 @@
+window.timelineFiles = [
+ {
+  date:"2026-03-11",
+  files:[
+   "uploads/2026-03-11/diagram-export-10-22-2025-5_02_24-PM.png","uploads/2026-03-11/Screenshot_20251009_230404_Clash Royale.jpg"
+  ]
+ },
+  {
+  date:"2026-03-10",
+  files:[
+   "uploads/2026-03-10/Marvel_Cinematic_Battle_Scene_Generated.mp4"
+  ]
+ }
+]
